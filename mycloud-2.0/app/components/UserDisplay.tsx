c21121058892d1e64ec09b185c8cb21e2c6b5b28
TadeJin <tadeas.jindra28@seznam.cz>
2026-03-09 16:35:57 +0100

    removed async from UserDisplay

--- mycloud-2.0/app/components/UserDisplay.tsx
+++ mycloud-2.0/app/components/UserDisplay.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { signOut } from "next-auth/react";
 
-export const LogOutButton = async () => {
+export const LogOutButton = () => {
     return <button className = "p-1 outline-1 outline-black hover:bg-gray-400 cursor-pointer" onClick = {() => signOut({ callbackUrl: "/" })}>Log out</button>
 }
