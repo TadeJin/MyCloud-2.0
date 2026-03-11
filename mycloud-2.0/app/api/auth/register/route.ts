eacd209b6e80e09dd3a015caf87c8a2a07871aa2
TadeJin <tadeas.jindra28@seznam.cz>
2026-03-11 18:30:41 +0100

    added folder creation on new user register

--- mycloud-2.0/app/api/auth/register/route.ts
+++ mycloud-2.0/app/api/auth/register/route.ts
@@ -1,6 +1,8 @@
 import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 import argon2 from "argon2";
+import { mkdir } from "fs/promises";
+import path from "path";
 
 export const POST = async (req: Request) => {
     const {email, password} = await req.json();
@@ -12,12 +14,15 @@ export const POST = async (req: Request) => {
         );
     }
 
-    await prisma.user.create({
+    const user = await prisma.user.create({
         data: {
             email: email,
             password: await argon2.hash(password),
         },
     });
+
+    const dirPath = path.join(process.cwd(), "public", "test_storage", user.id.toString());
+    await mkdir(dirPath);
 
     return NextResponse.json({ message: "User created" }, { status: 201 });
 }
