9b958db94afa720641b47b7fb8f2c04fa6b32182
TadeJin <tadeas.jindra28@seznam.cz>
2026-03-11 18:31:12 +0100

    moved file operations api

--- mycloud-2.0/app/api/files/fetchFiles/route.ts
+++ mycloud-2.0/app/api/files/fetchFiles/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/app/lib/prisma";
 import { getServerSession } from "next-auth"
 import { NextResponse } from "next/server";
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "../../auth/[...nextauth]/route";
 
 export const GET = async () => {
     const session = await getServerSession(authOptions);
--- mycloud-2.0/app/api/files/upload/route.ts
+++ mycloud-2.0/app/api/files/upload/route.ts
@@ -3,7 +3,7 @@ import { writeFile } from "fs/promises";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import path from "path";
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "../../auth/[...nextauth]/route";
 
 export const POST = async (req: Request) => {
     const session = await getServerSession(authOptions);
@@ -29,10 +29,12 @@ export const POST = async (req: Request) => {
         data: {
             name: file.name,
             userId: session.user.id,
+            type: file.type,
+            size: file.size
         },
     });
 
-    const filePath = path.join(process.cwd(), "public", "test_storage", file.name);
+    const filePath = path.join(process.cwd(), "public", "test_storage", session.user.id.toString(), file.name);
     const buffer = Buffer.from(await file.arrayBuffer());
 
     await writeFile(filePath, buffer);
