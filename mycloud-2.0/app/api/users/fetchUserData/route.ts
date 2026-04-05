0fb11d448090915e20ae6a152447544b9cf70c73
TadeJin <tadeas.jindra28@seznam.cz>
2026-04-05 21:12:50 +0200

    fetchUserData API fix

--- mycloud-2.0/app/api/users/fetchUserData/route.ts
+++ mycloud-2.0/app/api/users/fetchUserData/route.ts
@@ -1,9 +1,10 @@
 import prisma from "@/app/lib/prisma";
 import { getServerSession } from "next-auth"
 import { NextResponse } from "next/server";
+import { authOptions } from "../../auth/[...nextauth]/route";
 
 export const GET = async () => {
-    const session = await getServerSession();
+    const session = await getServerSession(authOptions);
 
     if (!session) {
         return NextResponse.json(
